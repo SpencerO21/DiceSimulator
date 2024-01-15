9b1cf129eefbc2b73031877ebea8207987142f1f
spencerolson <spencerolson@SPENCEROLSOA203>
2024-01-15 13:20:26 -0700

    Add project files.

---
+++ DiceSimulator/DiceRoll.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceSimulator
+{
+    internal class DiceRoll
+    {
+        public DiceRoll() { 
+            
+        }
+
+        public int[] rollTheDice(int numRolls)
+        {
+            Random random = new Random();
+            int[] diceRolls = new int[11];
+            int dice1 = 0;
+            int dice2 = 0;
+            int total = 0;
+            // Loop for the number of times the user wants to roll
+            for (int i = 0; i < numRolls; i++)
+            {
+                // For each roll, generate a random number for each dice, add the number together
+                dice1 = random.Next(1, 13);
+                dice2 = random.Next(1, 13);
+                total = dice1 + dice2;
+
+                switch (total)
+                {
+                    case 2:
+                        diceRolls[0]++;
+                        break;
+                    case 3:
+                        diceRolls[1]++;
+                        break;
+                    case 4:
+                        diceRolls[2]++; break;
+                    case 5:
+                        diceRolls[3]++; break;
+                    case 6:
+                        diceRolls[4]++; break;
+                    case 7:
+                        diceRolls[5]++; break;
+                    case 8:
+                        diceRolls[6]++; break;
+                    case 9:
+                        diceRolls[7]++; break;
+                    case 10:
+                        diceRolls[8]++; break;
+                    case 11:
+                        diceRolls[9]++;
+                        break;
+                    case 12:
+                        diceRolls[10]++; break;
+                }
+
+            }
+            return diceRolls;
+        }
+    }
+}
---
+++ DiceSimulator/Program.cs
@@ -0,0 +1,33 @@
+﻿using DiceSimulator;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        System.Console.WriteLine("How many times do you want to roll the dice?");
+        int numRolls = int.Parse(System.Console.ReadLine());
+        DiceRoll diceRoll = new DiceRoll();
+
+        int[] results = diceRoll.rollTheDice(numRolls);
+        // Print the simulation results
+        PrintSimulationResults(results, numRolls);
+
+        System.Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    }
+
+    static void PrintSimulationResults(int[] results, int numRolls)
+    {
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+
+        // Print the total number of rolls
+        System.Console.WriteLine($"Total number of rolls = {numRolls}.");
+
+        // Print the histogram
+        for (int i = 2; i <= 12; i++)
+        {
+            int percentage = results[i - 2] * 100 / numRolls;
+            System.Console.WriteLine($"{i}: {new string('*', percentage)}");
+        }
+    }
+}
